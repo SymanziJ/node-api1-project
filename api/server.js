eba4599f81910c8754193473b66ec77953a7c091
SymanziJ <97126299+SymanziJ@users.noreply.github.com>
2022-07-11 19:56:27 -0400

    put user endpoint

--- api/server.js
+++ api/server.js
@@ -14,7 +14,8 @@ server.get('/api/users', (req, res) => {
         .catch(err => {
             res.status(500).json({
                 message: 'error getting users',
-                err: err.message
+                err: err.message,
+                stack: err.stack
             })
         })
 });
@@ -22,24 +23,27 @@ server.get('/api/users', (req, res) => {
 server.get('/api/users/:id', (req, res) => {
     const { id } = req.params;
     Users.findById(id).then(user => {
-        if(user === null || user === undefined) {
+        if(user == null) {
             res.status(404).json({ message: 'does not exist'})
         } else {
             res.json(user);
         }
-    });
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting users',
+            err: err.message,
+            stack: err.stack
+        })
+    })
 });
 
 server.post('/api/users', (req, res) => {
     let body = req.body;
-    if(body.name == null) {
+    if(!body.name || !body.bio) {
         res.status(400).json({ message: "Please provide name and bio for the user" });
         return;
     } 
-    if(body.bio == null) {
-        res.status(400).json({ message: "Please provide name and bio for the user" });
-        return;
-    }
     Users.insert(body)
         .then(user => {
             res.status(201).json(user);
@@ -50,6 +54,26 @@ server.delete('/api/users/:id', (req, res) => {
     const { id } = req.params;
     Users.remove(id).then(user => {
         if (user == null) {
+            res.status(404).json({ message: 'does not exist'})
+        } else {
+            res.json(user);
+        }
+    })
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    let body = req.body;
+    if(body.name == null) {
+        res.status(400).json({ message: "Please provide name and bio for the user" });
+        return;
+    } 
+    if(body.bio == null) {
+        res.status(400).json({ message: "Please provide name and bio for the user" });
+        return;
+    }
+    Users.update(id, body).then(user => {
+        if(user == null) {
             res.status(404).json({ message: 'does not exist'})
         } else {
             res.json(user);
